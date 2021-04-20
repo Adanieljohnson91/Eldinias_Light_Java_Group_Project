da3ce5d3cd51731a2b5613bba7542b8726491fd7
AdanielJohnson91 <dan.johnson1022@gmail.com>
2021-04-20 14:20:12 -0700

    Scanner

--- src/com/fourforfour/eldanialight/Main.java
+++ src/com/fourforfour/eldanialight/Main.java
@@ -1,7 +1,17 @@
 package com.fourforfour.eldanialight;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+        Scanner myScanner = new Scanner(System.in); // Make scanner obj
+
+
+        while(true){
+            System.out.print("$ ");
+            String inputString = myScanner.nextLine();
+            System.out.println(inputString);
+        }
     }
 }
