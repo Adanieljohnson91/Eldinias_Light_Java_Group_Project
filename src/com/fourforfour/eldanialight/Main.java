3807e12c068bcc2c4c7a1b41058fd25537919882
AdanielJohnson91 <dan.johnson1022@gmail.com>
2021-04-21 07:34:21 -0700

    psudocode and notes for game flow

--- src/com/fourforfour/eldanialight/Main.java
+++ src/com/fourforfour/eldanialight/Main.java
@@ -6,12 +6,18 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
         Scanner myScanner = new Scanner(System.in); // Make scanner obj
-
+        //When the game is started we will have to
+        // 1.) Create our character (Should this be like a menu? Can we clear the screen on each selection?
+        // 2.) Thread to start intro story?
+        Game game = new Game();
         String inputString = "";
         while(true){
+            //Invalid entry should result in list of valid entries.
             System.out.print("$ ");
             inputString = myScanner.nextLine();
-            System.out.println(inputString);
+            //Game is always taking in commands play is the master controller.
+
+            game.play(inputString);
 
 
 
