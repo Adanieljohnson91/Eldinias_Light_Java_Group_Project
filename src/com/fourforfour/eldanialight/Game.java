ee2ea4481e21e4639bb88892dd9326684442ec25
AdanielJohnson91 <dan.johnson1022@gmail.com>
2021-04-22 09:22:51 -0700

    shannons player creator

--- src/com/fourforfour/eldanialight/Game.java
+++ src/com/fourforfour/eldanialight/Game.java
@@ -9,7 +9,7 @@ import java.util.HashMap;
 import java.util.List;
 
 public class Game {
-    public static Area currentArea = new Area("The Beginning", new AreaList("area1"), AreaKommands.worldCommand);
+    public static Area currentArea = new Area("The Beginning", new AreaList("area1"), AreaKommands.battleCommand);
     Character character;
     World world = new World();
     //Commands command ... each command has a list of commands that it can take in
--- src/com/fourforfour/eldanialight/Main.java
+++ src/com/fourforfour/eldanialight/Main.java
@@ -7,13 +7,13 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Player.createPlayer();
+       ;
         System.out.println("Hello, World!");
         Scanner myScanner = new Scanner(System.in); // Make scanner obj
         //When the game is started we will have to
         // 1.) Create our character (Should this be like a menu? Can we clear the screen on each selection?
         // 2.) Thread to start intro story?
-        Game game = new Game(new Character());
+        Game game = new Game(Player.createPlayer());
         String inputString = "";
         while(true){
             //Invalid entry should result in list of valid entries
