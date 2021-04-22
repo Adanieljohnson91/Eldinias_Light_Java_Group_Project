b37e5eb42697d9e9d6c890476bd1e1cf734c8022
Tamarris Jenkins <tamarris.jenkins@gmail.com>
2021-04-22 12:29:25 -0400

    added  methods fro shopkeepr

--- src/com/fourforfour/eldanialight/characters/Boss.java
+++ src/com/fourforfour/eldanialight/characters/Boss.java
@@ -1,4 +1,34 @@
 package com.fourforfour.eldanialight.characters;
 
-public class Boss extends Character {
-}
+public class Boss extends Character implements BattleActions {
+
+    public Boss(String name, int health, int strength, int defense ) {
+
+
+        setName(name);
+        setHealth(health);
+        setStrength(strength);
+        setDefense(defense);
+
+    }
+
+    @Override
+    public void attack() {
+
+    }
+
+    @Override
+    public void run() {
+
+    }
+
+    @Override
+    public void defend() {
+
+    }
+
+    @Override
+    public void use() {
+
+    }
+}//EOC
--- src/com/fourforfour/eldanialight/characters/ShopNPC.java
+++ src/com/fourforfour/eldanialight/characters/ShopNPC.java
@@ -1,12 +1,25 @@
 package com.fourforfour.eldanialight.characters;
 
-public class ShopNPC extends Character {
+public class ShopNPC extends Character implements InteractActions {
 
 
     public ShopNPC(int bezos){
         setBezos(bezos);
     }
 
+    @Override
+    public void talk() {
+        System.out.println(" Hello traveler what can I do for you?");
+    }
 
+    @Override
+    public void buy() {
+
+    }
+
+    @Override
+    public void sell() {
+        System.out.println("");
+    }
 
 }//EOC
