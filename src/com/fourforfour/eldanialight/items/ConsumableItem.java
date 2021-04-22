bfb2b362c42eab7908e0773c0be91de2dd048090
Marco Bragado <marcobragado.cs@gmail.com>
2021-04-22 03:58:24 -0700

    MB#132 hp and mp attributes and methods initialized

--- src/com/fourforfour/eldanialight/items/ConsumableItem.java
+++ src/com/fourforfour/eldanialight/items/ConsumableItem.java
@@ -1,4 +1,15 @@
 package com.fourforfour.eldanialight.items;
 
 public class ConsumableItem {
+
+    private int healthPotion = 50;
+    private int manaPotion = 50;
+
+    public int useHealthPotion(int currentHP) {
+        return healthPotion + currentHP;
+    }
+
+    public int useManaPotion(int currentMP) {
+        return manaPotion + currentMP;
+    }
 }
