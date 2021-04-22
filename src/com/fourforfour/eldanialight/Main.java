db194ae51336f591625a0babaa8b319aec4124d2
Tamarris Jenkins <tamarris.jenkins@gmail.com>
2021-04-22 13:19:10 -0400

    added opening story to the game. edited Shop and Quest NPCS

--- src/com/fourforfour/eldanialight/Main.java
+++ src/com/fourforfour/eldanialight/Main.java
@@ -7,15 +7,17 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-       ;
-        System.out.println("Hello, World!");
+        ;
+        System.out.println("The once peaceful land of Eldina has recently been taken over by the evil warlock Tyroneious" +
+                "the Black who has cast a darkness upon the land.\n You are the one chosen to defeat Tyroneious and his " +
+                "Army to restore Eldina back to its peaceful ways. \n");
         Scanner myScanner = new Scanner(System.in); // Make scanner obj
         //When the game is started we will have to
         // 1.) Create our character (Should this be like a menu? Can we clear the screen on each selection?
         // 2.) Thread to start intro story?
         Game game = new Game(Player.createPlayer());
         String inputString = "";
-        while(true){
+        while (true) {
             //Invalid entry should result in list of valid entries
             //Game is always taking in commands play is the master controller.
             System.out.println("Enter a command: ");
@@ -25,8 +27,7 @@ public class Main {
             game.play(inputString);
 
 
-
-            if(inputString.equals("exit()")){
+            if (inputString.equals("exit()")) {
                 System.exit(0);
             }
         }
