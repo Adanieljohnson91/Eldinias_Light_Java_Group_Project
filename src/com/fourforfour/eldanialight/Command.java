9d7efbd125b9c217a7471dc85cbf19b585398eb8
AdanielJohnson91 <dan.johnson1022@gmail.com>
2021-04-21 17:36:56 -0700

    commting to pull

--- src/com/fourforfour/eldanialight/Command.java
+++ src/com/fourforfour/eldanialight/Command.java
@@ -1,25 +1,18 @@
 package com.fourforfour.eldanialight;
 
-enum Commands {
-    VIEW_MAP("view map"),
-    ACCEPT("accept"),
-    IGNORE("ignore"),
-    QUIT("quit"),
-    INVALID_CMD("invalid command"),
-    ATTACK("attack"),
-    RUN("run");
-
-
-    // This class will be used to check commands
-    private  String command;
-
-    Commands(String command){
-        this.command = command;
-    };
-
-    public String command(){
-        return this.command;
-    }
-
-
+public enum Command {
+    VIEW,
+    GO,
+    RUN,
+    ATTACK,
+    BUY,
+    SELL,
+    OPEN,
+    CLOSE,
+    EXIT,
+    ACCEPT,
+    IGNORE,
+    LEAVE,
+    VENTURE,
+    ITEM
 }
--- src/com/fourforfour/eldanialight/areas/Area.java
+++ src/com/fourforfour/eldanialight/areas/Area.java
@@ -1,4 +1,46 @@
 package com.fourforfour.eldanialight.areas;
 
+import com.fourforfour.eldanialight.AreaKommands;
+import com.fourforfour.eldanialight.Command;
+
+import java.util.HashMap;
+import java.util.List;
+
 public class Area {
+
+    private String name;
+    private HashMap<String, Character> characters = new HashMap<>();
+    private AreaList areas;
+    private List<Command> command;
+
+    public AreaList getAreas(){
+        return this.areas;
+    }
+
+    public Area(String name, AreaList areas, List<Command> command){
+        this.name = name;
+        this.areas = areas;
+        this.command = command;
+    }
+
+    public void printCommands(){
+        for(Command cmd : command){
+            System.out.println(cmd);
+        }
+    }
+
+    public void commands(String input){
+        if(command.contains(Command.valueOf(input))){
+            AreaKommands.commands(Command.valueOf(input));
+            return;
+        }
+        System.out.println("ERROR: Please input the following: ");
+        printCommands();
+
+    }
+
+    public void viewAreas(){
+        areas.view();
+    }
+
 }
